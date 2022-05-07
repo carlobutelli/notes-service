5c2c4c2ea9509418eea39cfd579eac6a6d4466fd
becbutelli <c.butelli@be-tse.it>
2022-05-07 16:20:02 +0200

    added render template

--- api/user/views.py
+++ api/user/views.py
@@ -7,7 +7,7 @@ from api.user.handler import create_user
 user = Blueprint("user", __name__, url_prefix="/user")
 
 
-@user.route("", methods=['POST'])
+@user.route('', methods=['POST'])
 @swag_from("/api/docs/user.yml")
 def user_create_route():
     if request.method == "POST":
