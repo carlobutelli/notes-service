38b6d7787651d082f0baeab6851ea0f0224a05c7
becbutelli <c.butelli@be-tse.it>
2022-05-07 16:19:52 +0200

    added render template

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+from flask import Blueprint, render_template
+
+auth = Blueprint("auth", __name__,  url_prefix="/auth")
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
